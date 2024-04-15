4f68605aa98319392e35b555cbd55672d98416c1
Iannn <pristianb007@gmail.com>
2024-04-15 15:13:14 +0700

    You know what? The main cause why is it causing a bug is bcs the button type for login must be a submit :<

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { supabase } from '@/lib/supabase';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
       <h1 className='text-3xl font-bold'>Hi Supabase!</h1>
+      <h2 className='text-xl m-5'>
+        Click the following link to see the difference of your DB
+      </h2>
+      <ul className='flex flex-col gap-5' id='nav'>
+        <li>
+          <Link href='/new-view'>New View</Link>
+        </li>
+        <li>
+          <Link href='/auth-client'>Auth Client</Link>
+        </li>
+        <li>
+          <Link href='/auth-server'>Auth Server</Link>
+        </li>
+        <li>Test</li>
+      </ul>
     </>
   );
 }
