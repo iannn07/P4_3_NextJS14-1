6458799e002f3e150b08c322e9645d48c5e127e9
Iannn <pristianb007@gmail.com>
2024-04-15 13:08:18 +0700

    Auth Client - LOT OF VULNERABLE :(

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  // let session;
+
+  // setTimeout(async () => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  // session = currentSession;
+  // }, 3600000);
+
+  console.log(session);
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ['/auth-client/:path*'],
+};
